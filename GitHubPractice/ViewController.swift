71f5413122c62c2b86e5aee41590eb9b51a561a9
sunilgondil <sunilgondildcoder@gmail.com>
2021-08-29 16:29:03 +0530

    first commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Sunil Gondil on 29/08/21.
+//  Copyright © 2021 NotOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
